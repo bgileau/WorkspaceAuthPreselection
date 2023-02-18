fbf9c13e77c0e90a8a747e44e8ae3b18b35c1060
bgileau <95985118+bgileau@users.noreply.github.com>
2023-02-18 11:35:55 -0700

    updated to include better user options

--- Workspace Auth Preselection.user.js
+++ Workspace Auth Preselection.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Workspace Auth Preselection
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  Preselects a specific authentication method from a dropdown on page load to make logging in faster/less annoying.
 // @include        https://workspace.*/logon/LogonPoint/tmindex.html
 // ==/UserScript==
@@ -34,12 +34,14 @@
             option.selected = true;
         }
     }
-
-    const timeout_period = 5000  // attempts this script for at most 5 seconds (testing indicated 3 seconds was more than sufficient, but this can be adjusted)
+  
+  	const timeout_period = 5000 // attempts this script for at most 5 seconds (testing indicated 3 seconds was more than sufficient, but this can be adjusted)
+    const id_of_element = "domain" // use inspect element on the page if use-case differs
+    const desired_dropdown_value = "Azure MFA" // the value in the dropdown you want to be preselected
 
     // Wait for the "domain" select element to become available
-    waitForElement('#domain', timeout_period, function(domainSelect) {
+    waitForElement(`#${id_of_element}`, timeout_period, function(selected_id_element) {
         // Set the "Azure MFA" option as the preselected option
-        preselectOption(domainSelect, "Azure MFA");
+        preselectOption(selected_id_element, desired_dropdown_value);
     });
 })();
